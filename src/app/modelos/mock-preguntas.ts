50204a979e8da4871a717ed05ccba5057bf25cfa
sergiogarciaa <sergioogarciabarrera@gmail.com>
2023-10-17 10:11:49 +0200

    act

--- src/app/modelos/mock-preguntas.ts
+++ src/app/modelos/mock-preguntas.ts
@@ -1,12 +1,12 @@
 import { preguntas } from '../preguntas/preguntas';
 
 export const PREGUNTAS: preguntas[] = [
-  { id: 1, enunciado: '¿Cómo se define TypeScript?', respuesta: 'TS' },
-  { id: 2, enunciado: '¿Cuál es la principal diferencia de TypeScript respecto de JavaScript?', respuesta: 'La abreviacion de codigo' },
-  { id: 3, enunciado: 'Muestra la lista de tipos básicos de TypeScript.', respuesta: '...' },
-  { id: 4, enunciado: '¿Con qué tres elementos podemos crear un tipo personalizado en TypeScript?', respuesta: 'm g c' },
-  { id: 5, enunciado: '¿Puedo ejecutar TypeScript en un navegador? Explica tu respuesta.', respuesta: 'No, tiene que ser compilado'},
-  { id: 6, enunciado: 'Entonces, ¿cómo es que puedo ver con las herramientas del desarrollador del navegador los archivo ts?', respuesta: 'No, tiene que ser compilado'},
-  { id: 7, enunciado: '¿Para qué sirve el archivo tsconfig.json?', respuesta: 'No, tiene que ser compilado'}
+  { id: 1, enunciado: '¿Cómo se define TypeScript?', respuesta: 'Como un lenguaje orientado a objetos' },
+  { id: 2, enunciado: '¿Cuál es la principal diferencia de TypeScript respecto de JavaScript?', respuesta: 'TypeScript dispone de una escritura estática, mientras que JavaScript es un lenguaje dinámico.' },
+  { id: 3, enunciado: 'Muestra la lista de tipos básicos de TypeScript.', respuesta: 'Cadenas de texto (strings), Números (number), Booleanos (true & false) ,Arrays, Tuplas, Enumerados (enum)Void.' },
+  { id: 4, enunciado: '¿Con qué tres elementos podemos crear un tipo personalizado en TypeScript?', respuesta: '...' },
+  { id: 5, enunciado: '¿Puedo ejecutar TypeScript en un navegador? Explica tu respuesta.', respuesta: '...'},
+  { id: 6, enunciado: 'Entonces, ¿cómo es que puedo ver con las herramientas del desarrollador del navegador los archivo ts?', respuesta: '...'},
+  { id: 7, enunciado: '¿Para qué sirve el archivo tsconfig.json?', respuesta: '...'}
 
 ];
